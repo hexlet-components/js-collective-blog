624fb3b5bb1544aab4551849ee9b4fc575c0b2ca
Kirill Mokevnin <mokevnin@gmail.com>
2016-11-18 22:28:48 +0300

    add new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -21,4 +21,24 @@ describe('requests', () => {
       .get('/posts/1');
     expect(res).toHaveHTTPStatus(200);
   });
+
+  it('GET /undefined', async () => {
+    const res = await request(app)
+      .get('/undefined');
+    expect(res).toHaveHTTPStatus(404);
+  });
+
+  it('GET /users/new', async () => {
+    const res = await request(app)
+      .get('/users/new');
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  it('POST /users', async () => {
+    const res = await request(app)
+      .post('/users')
+      .type('form')
+      .send({ nickname: 'nickname', password: 'qwer' });
+    expect(res).toHaveHTTPStatus(302);
+  });
 })
